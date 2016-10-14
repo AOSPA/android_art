dfee1e05987dcbe4e17770407d25b6d02bb8ad72
Ícaro Hoff <icarohoff@gmail.com>
2016-10-14 09:22:31 -0300

    art: pump optimization levels all the way up
    Debug: -O3
    Non-debug: -Ofast
    
    Change-Id: I3089b9edbec1d5678d588a331fddd6345e9f3dac
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- build/Android.common_build.mk
+++ build/Android.common_build.mk
@@ -40,8 +40,8 @@ ART_BUILD_HOST_DEBUG ?= true
 endif
 
 # Set this to change what opt level Art is built at.
-ART_DEBUG_OPT_FLAG ?= -O2
-ART_NDEBUG_OPT_FLAG ?= -O3
+ART_DEBUG_OPT_FLAG ?= -O3
+ART_NDEBUG_OPT_FLAG ?= -Ofast
 
 # Enable the static builds only for checkbuilds.
 ifneq (,$(filter checkbuild,$(MAKECMDGOALS)))
