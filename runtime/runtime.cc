MotorolaMobilityLLC/art

Pick up dex2oat command line consistently
The framework and the runtime use different command lines
for dex2oat. The framework's version is correct. Switch the
runtime to using the same version.

Change-Id: I64a37992923d30715bf9015299e1896068062396
Signed-off-by: ubanerji <ubanerji@motorola.com>
Reviewed-on: /833512
SLTApproved: Slta Waiver <sltawvr@motorola.com>
SME-Granted: SME Approvals Granted
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: Xin Guan <a18772@motorola.com>
SLTApproved: Xin Guan <a18772@motorola.com>
Reviewed-by: Jing Ji <a5705c@motorola.com>
Submit-Approved: Jira Key <jirakey@motorola.com>
(cherry picked from commit eab9e0b845908ea5e9986af9d8f10d1407a1d253)
Reviewed-on: /837657
Reviewed-on: /859147

runtime/runtime.cc
@@ -23,6 +23,12 @@
 #include <sys/prctl.h>
 #endif
 
+// BEGIN Motorola, ubanerji, 03/16/2016, IKSWM-6668
+// Needed to read system properties.
+#ifdef HAVE_ANDROID_OS
+#include "cutils/properties.h"
+#endif
+// END IKSWM-6668
 #include <signal.h>
 #include <sys/syscall.h>
 #include "base/memory_tool.h"
@@ -1955,10 +1961,21 @@ void Runtime::AddCurrentRuntimeFeaturesAsDex2OatArguments(std::vector<std::strin
   instruction_set += GetInstructionSetString(kRuntimeISA);
   argv->push_back(instruction_set);
 
-  std::unique_ptr<const InstructionSetFeatures> features(InstructionSetFeatures::FromCppDefines());
-  std::string feature_string("--instruction-set-features=");
-  feature_string += features->GetFeatureString();
-  argv->push_back(feature_string);
+  // BEGIN Motorola, ubanerji, 03/16/2016, IKSWM-6668
+#ifdef HAVE_ANDROID_OS
+  std::string dex2oat_isa_features_key;
+  dex2oat_isa_features_key += "dalvik.vm.isa." + instruction_set + ".features";
+  char dex2oat_isa_features[PROPERTY_VALUE_MAX];
+  bool have_dex2oat_isa_features = property_get(dex2oat_isa_features_key.c_str(),
+                                                 dex2oat_isa_features, NULL) > 0;
+
+  if (have_dex2oat_isa_features) {
+    std::string feature_string("--instruction-set-features=");
+    feature_string += std::string(dex2oat_isa_features);
+    argv->push_back(feature_string);
+  }
+#endif
+  // END IKSWM-6668
 }
 
 void Runtime::CreateJit() {

AOSPA/android_art

Signed-off-by: Alex Naidis <alex.naidis@linux.com>

runtime/runtime.cc
@@ -23,6 +23,12 @@
 #include <sys/prctl.h>
 #endif
 
+// BEGIN Motorola, ubanerji, 03/16/2016, IKSWM-6668
+// Needed to read system properties.
+#ifdef __ANDROID__
+#include "cutils/properties.h"
+#endif
+// END IKSWM-6668
 #include <signal.h>
 #include <sys/syscall.h>
 #include "base/memory_tool.h"
@@ -1947,10 +1953,21 @@ void Runtime::AddCurrentRuntimeFeaturesAsDex2OatArguments(std::vector<std::strin
   instruction_set += GetInstructionSetString(kRuntimeISA);
   argv->push_back(instruction_set);
 
-  std::unique_ptr<const InstructionSetFeatures> features(InstructionSetFeatures::FromCppDefines());
-  std::string feature_string("--instruction-set-features=");
-  feature_string += features->GetFeatureString();
-  argv->push_back(feature_string);
+  // BEGIN Motorola, ubanerji, 03/16/2016, IKSWM-6668
+#ifdef __ANDROID__
+  std::string dex2oat_isa_features_key;
+  dex2oat_isa_features_key += "dalvik.vm.isa." + instruction_set + ".features";
+  char dex2oat_isa_features[PROPERTY_VALUE_MAX];
+  bool have_dex2oat_isa_features = property_get(dex2oat_isa_features_key.c_str(),
+                                                 dex2oat_isa_features, NULL) > 0;
+
+  if (have_dex2oat_isa_features) {
+    std::string feature_string("--instruction-set-features=");
+    feature_string += std::string(dex2oat_isa_features);
+    argv->push_back(feature_string);
+  }
+#endif
+  // END IKSWM-6668
 }
 
 void Runtime::CreateJit() {