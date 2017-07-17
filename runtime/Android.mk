d82836e5149eea08eda92211bb0e6c9586f7367d
TheStrix <parthbhatia98@gmail.com>
2017-07-17 18:35:11 +0530

    runtime: Support cortex-a73 for 32 and 64 bit
    Change-Id: I4b8c8a050e464824d2a5803e80179cada14c5edf

--- runtime/Android.mk
+++ runtime/Android.mk
@@ -389,7 +389,7 @@ LIBART_HOST_DEFAULT_INSTRUCTION_SET_FEATURES := default
 LIBART_TARGET_DEFAULT_INSTRUCTION_SET_FEATURES := default
 2ND_LIBART_TARGET_DEFAULT_INSTRUCTION_SET_FEATURES := default
 ifeq ($(DEX2OAT_TARGET_ARCH),arm)
-  ifneq (,$(filter $(DEX2OAT_TARGET_CPU_VARIANT),cortex-a15 krait kryo denver))
+  ifneq (,$(filter $(DEX2OAT_TARGET_CPU_VARIANT),cortex-a15 krait kryo denver cortex-a73))
     LIBART_TARGET_DEFAULT_INSTRUCTION_SET_FEATURES := atomic_ldrd_strd,div
   else
     ifneq (,$(filter $(DEX2OAT_TARGET_CPU_VARIANT),cortex-a7))
@@ -398,7 +398,7 @@ ifeq ($(DEX2OAT_TARGET_ARCH),arm)
   endif
 endif
 ifeq ($(2ND_DEX2OAT_TARGET_ARCH),arm)
-  ifneq (,$(filter $(DEX2OAT_TARGET_CPU_VARIANT),cortex-a15 krait kryo denver))
+  ifneq (,$(filter $(DEX2OAT_TARGET_CPU_VARIANT),cortex-a15 krait kryo denver cortex-a73))
     2ND_LIBART_TARGET_DEFAULT_INSTRUCTION_SET_FEATURES := atomic_ldrd_strd,div
   else
     ifneq (,$(filter $(DEX2OAT_TARGET_CPU_VARIANT),cortex-a7))
