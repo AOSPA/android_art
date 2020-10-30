addf2582f2ad3a7111d98a987276af0da94315e6
Paul Duffin <paulduffin@google.com>
2020-10-30 11:58:47 +0000

    [master] dexdump: fix xml output for jars containing multiple .dex files
    When an input file (.jar/.zip) contains multiple dex files their
    xml output goes into one api element. Previously, dexdump emitted an
    api element per input.
    
    Add a test that generates a multi-dex jar and checks the output.
    
    Bug: 172042691
    Test: art/test/dexdump/run-all-tests
    Change-Id: I28b60d5a4b536d5ed6feb05c2476d58c00a33b68

--- dexdump/dexdump.cc
+++ dexdump/dexdump.cc
@@ -1788,11 +1788,6 @@ static void processDexFile(const char* fileName,
     dumpFileHeader(pDexFile);
   }
 
-  // Open XML context.
-  if (gOptions.outputFormat == OUTPUT_XML) {
-    fprintf(gOutFile, "<api>\n");
-  }
-
   // Iterate over all classes.
   char* package = nullptr;
   const u4 classDefsSize = pDexFile->GetHeader().class_defs_size_;
@@ -1814,11 +1809,6 @@ static void processDexFile(const char* fileName,
   if (package != nullptr) {
     fprintf(gOutFile, "</package>\n");
     free(package);
-  }
-
-  // Close XML context.
-  if (gOptions.outputFormat == OUTPUT_XML) {
-    fprintf(gOutFile, "</api>\n");
   }
 }
 
@@ -1863,8 +1853,18 @@ int processFile(const char* fileName) {
   if (gOptions.checksumOnly) {
     fprintf(gOutFile, "Checksum verified\n");
   } else {
+    // Open XML context.
+    if (gOptions.outputFormat == OUTPUT_XML) {
+      fprintf(gOutFile, "<api>\n");
+    }
+
     for (size_t i = 0, n = dex_files.size(); i < n; i++) {
       processDexFile(fileName, dex_files[i].get(), i, n);
+    }
+
+    // Close XML context.
+    if (gOptions.outputFormat == OUTPUT_XML) {
+      fprintf(gOutFile, "</api>\n");
     }
   }
   return 0;
