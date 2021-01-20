4406e339fa80543cd8e1c7909c53944c20bb5296
Nikita Iashchenko <nikitai@google.com>
2021-01-20 21:52:54 +0000

    Add Math.multiplyHigh intrinsic
    Test: ./art/test/testrunner/testrunner.py --target  --optimizing --64 -t 082-inline-execute
    Test: ./art/test/testrunner/testrunner.py --host --optimizing --64 -t 082-inline-execute
    Change-Id: I4b6cafa8b9e513eca7c5c139440024d87a7ef758
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>

--- compiler/optimizing/intrinsics_arm64.cc
+++ compiler/optimizing/intrinsics_arm64.cc
@@ -3428,6 +3428,23 @@ void IntrinsicCodeGeneratorARM64::VisitLongDivideUnsigned(HInvoke* invoke) {
   GenerateDivideUnsigned(invoke, codegen_);
 }
 
+void IntrinsicLocationsBuilderARM64::VisitMathMultiplyHigh(HInvoke* invoke) {
+  CreateIntIntToIntLocations(allocator_, invoke);
+}
+
+void IntrinsicCodeGeneratorARM64::VisitMathMultiplyHigh(HInvoke* invoke) {
+  LocationSummary* locations = invoke->GetLocations();
+  MacroAssembler* masm = codegen_->GetVIXLAssembler();
+  DataType::Type type = invoke->GetType();
+  DCHECK(type == DataType::Type::kInt64);
+
+  Register x = RegisterFrom(locations->InAt(0), type);
+  Register y = RegisterFrom(locations->InAt(1), type);
+  Register out = RegisterFrom(locations->Out(), type);
+
+  __ Smulh(out, x, y);
+}
+
 UNIMPLEMENTED_INTRINSIC(ARM64, ReferenceGetReferent)
 
 UNIMPLEMENTED_INTRINSIC(ARM64, StringStringIndexOf);
