55ba9def053be5c31b3a8cbb0edd0312387f24e3
Vladimir Marko <vmarko@google.com>
2021-02-05 15:55:47 +0000

    [master] Fix Reference.refersTo() intrinsic for heap poisoning.
    Test: ART_HEAP_POISONING=true m test-art-host-gtest
    Test: ART_HEAP_POISONING=true testrunner.py --host --optimizing
    Test: run-gtests.sh # with ART_HEAP_POISONING=true
    Test: testrunner.py --target --optimizing # with ART_HEAP_POISONING=true
    Bug: 172573708
    Change-Id: I2e09b49813bdad40c1e18bf5681ba2094c5a40c1
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>

--- compiler/optimizing/intrinsics_arm64.cc
+++ compiler/optimizing/intrinsics_arm64.cc
@@ -2884,6 +2884,7 @@ void IntrinsicCodeGeneratorARM64::VisitReferenceRefersTo(HInvoke* invoke) {
 
   MemOperand field = HeapOperand(obj, referent_offset);
   codegen_->LoadAcquire(invoke, tmp, field, /*needs_null_check=*/ true);
+  codegen_->GetAssembler()->MaybeUnpoisonHeapReference(tmp);
 
   __ Cmp(tmp, other);
 
