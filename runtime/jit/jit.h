fc54a4e2ac926e0cb908194cd2fba130dc755082
Nicolas Geoffray <ngeoffray@google.com>
2021-11-29 14:47:21 +0000

    [master] Handle baseline/optimized compilation kind in the runtime.
    It used to be adjusted in the compiler, but that does not work anymore
    as the compiler now always requests a baseline compilation to have a
    profiling info.
    
    Test: 457-regs
    Bug: 146423102
    Change-Id: I522bc515ef1c6f8737ada311265a6f8e0f4db3fd

--- runtime/jit/jit.h
+++ runtime/jit/jit.h
@@ -203,6 +203,7 @@ class JitCompilerInterface {
       REQUIRES_SHARED(Locks::mutator_lock_) = 0;
   virtual bool GenerateDebugInfo() = 0;
   virtual void ParseCompilerOptions() = 0;
+  virtual bool IsBaselineCompiler() const;
 
   virtual std::vector<uint8_t> PackElfFileForJIT(ArrayRef<const JITCodeEntry*> elf_files,
                                                  ArrayRef<const void*> removed_symbols,
